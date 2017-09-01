be29dd5e8b56b10772b5be8417162b9d21f2d4e3
Andrew Cheung <cheungandrew5@gmail.com>
2017-09-01 01:50:16 -0500

    Added README and temperature feature

---
+++ README.md
@@ -0,0 +1,2 @@
+# get-weather-app
+An app that retrieves the current weather using the OpenWeatherMap API
--- weather.js
+++ weather.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
+    // Variables to create API URL
     var api = "http://api.openweathermap.org/data/2.5/weather?q=";
-    var city = "London";
-    var country = "uk";
     var apiKey ="&appid=782baf5721afe320477b796dedba34f7";
     var units = "&units=metric";
-    var url = api + city + "," + country + apiKey + units;
 
+    
     $("button#submit").click(function() {
-        $.getJSON(url, function(data) {
-            alert("Works");
-        })
+        // Getting city value from user
+        var city = document.getElementById("city").value;
+        // Creating API URL
+        var url = api + city + apiKey + units;
+
+        // Show Temperature
+        $.getJSON(url, function(weatherData) {
+            $(".temp").html(weatherData.main.temp + "&deg;C");
+        });
     });
 
 });
